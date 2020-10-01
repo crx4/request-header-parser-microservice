544fc920b6ff338e502d456333f2e93dff79b83b
Mevlut Canvar <mevlutcanvar@gmail.com>
2020-10-01 22:28:44 +0000

    v1.0.0

--- server.js
+++ server.js
@@ -18,13 +18,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.get('host'),
+    language: req.get('Accept-Language'),
+    software: req.get('user-agent')
+  });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
